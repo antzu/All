275bd56d5dcdedcf5737ea6af19fd0106b9ae435
antz <andres.aava@gmail.com>
2017-01-28 15:57:40 +0200

    ringid

--- lambikas/src/Legend.java
+++ lambikas/src/Legend.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 
@@ -25,7 +26,8 @@ public class Legend extends Application {
         l.setLayoutX(200);
         l.setLayoutY(200);
         p.getChildren().addAll(l);
-        ringid(p);
+        //ringid(p);
+        ristkulik(p);
     }
     public void ringid (Pane p){
 
@@ -36,6 +38,21 @@ public class Legend extends Application {
             c.setOnMouseEntered(event -> {
                 //c.setFill(Color.TRANSPARENT);
                 c.setVisible(false);
+            });
+        }
+    }
+    public void ristkulik (Pane p){
+        for (int i = 0; i < 20; i++) {
+            Rectangle r = new Rectangle(20, 40);
+            r.setX(Math.random()*400);
+            r.setY(Math.random()*400);
+            p.getChildren().addAll(r);
+            r.setOnMouseClicked(event ->{
+                r.setWidth(20/2);
+                r.setHeight(40/2);
+                r.setOnMouseClicked(event1 -> {
+                    r.setVisible(false);
+                });
             });
         }
     }
