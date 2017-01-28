eec4f2c536d7263e74750537c3c632f62d7fd90b
antz <andres.aava@gmail.com>
2017-01-28 14:51:40 +0200

    ringid

--- lambikas/src/Legend.java
+++ lambikas/src/Legend.java
@@ -1,34 +1,29 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Legend extends Application {
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Stage s = new Stage();
         Pane p = new Pane();
         Scene sc = new Scene(p, 500, 500);
         s.setScene(sc);
-        Circle ring1 = new Circle(50, 50, 30);
-        Circle ring2 = new Circle(450, 450, 30);
-        Circle ring3 = new Circle(200, 200, 30);
-        ring1.setVisible(false);
-        ring2.setVisible(false);
-        ring3.setVisible(false);
-        ring1.setOnMouseEntered(event -> {
-            ring1.setVisible(true);
-        });
-        ring2.setOnMouseEntered(event -> {
-            ring2.setVisible(true);
-        });
-        ring3.setOnMouseEntered(event -> {
-            ring3.setVisible(true);
-        });
-        p.getChildren().addAll(ring1, ring2, ring3);
         s.show();
+        ringid(p);
     }
-    public void hiir (){
+    public void ringid (Pane p){
+        for (int i = 0; i < 50; i++) {
+            Circle c = new Circle(Math.random()*400, Math.random()*400, 20);
+            c.setFill(Color.RED);
+            p.getChildren().addAll(c);
+            c.setOnMouseEntered(event -> {
+                c.setFill(Color.GREEN);
+            });
+        }
+
 
     }
 }
